7982fddf56cddeab5e950ee114a9312e94bc0c0f
A.Markelychev <a.markelychev@gmail.com>
2018-02-05 01:23:37 +0300

    minor changes

--- src/main.js
+++ src/main.js
@@ -3,12 +3,6 @@ import App from './App.vue'
 
 import VCalendar from 'v-calendar';
 import 'v-calendar/lib/v-calendar.min.css';
-
-import GetData from './components/GetData'
-import ShowData from './components/ShowData'
-
-Vue.component('GetData', GetData);
-Vue.component('ShowData', ShowData);
 Vue.use(VCalendar);
 
 new Vue({
